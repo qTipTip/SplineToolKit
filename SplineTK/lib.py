Fixed a bug in knot averages.

SplineTK/lib.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def index(x, t, eps=1.0E-14):
+def index(x, t, eps=np.spacing(1)):
     """
     Given an array t and a value x, return the index i such that
     t_i <= x < t_i+1. If x is equal to the last element of t, return the second to last index. If no
@@ -115,4 +115,4 @@ def knot_averages(t, p):
     :return: array
     """
 
-    return np.convolve(a=t[1:], v=np.ones(p + 1), mode='valid') / p
+    return np.convolve(a=t[1:-1], v=np.ones(p), mode='valid') / p

tests/test_knot_averages.py
@@ -8,7 +8,7 @@ def test_knot_averages_linear():
     p = 1
 
     computed_averages = knot_averages(t, p)
-    expected_averages = [3, 5, 6, 7]
+    expected_averages = [1, 2, 3, 3]
 
     np.testing.assert_almost_equal(computed_averages, expected_averages,
                                    err_msg=f'computed knot averages {computed_averages} do not agree with expected averages {expected_averages}')
@@ -19,7 +19,7 @@ def test_knot_averages_quadratic():
     p = 2
 
     computed_averages = knot_averages(t, p)
-    expected_averages = [3, 4, 5]
+    expected_averages = [1.5, 2.5, 3]
 
     np.testing.assert_almost_equal(computed_averages, expected_averages,
                                    err_msg=f'computed knot averages {computed_averages} do not agree with expected averages {expected_averages}')